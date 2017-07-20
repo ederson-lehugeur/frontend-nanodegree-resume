5650dba50c093fccd2c541902b3c9017617f225b
Éderson Lehugeur <ederson-lehugeur@tjmrs.jus.br>
2017-07-20 16:25:08 -0300

    Lesson 3: 12. Quiz: Manipulação de Strings Teste 2

--- Lesson_3/js/resumeBuilder.js
+++ Lesson_3/js/resumeBuilder.js
@@ -24,3 +24,26 @@ var formattedRole = HTMLheaderRole.replace("%data%", role);
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 
+/*
+var skills = ["awesomeness", "programming", "teaching", "JS"];
+var numbers = [0, 1, 2, 3];
+
+$("#main").append(skills);
+$("#main").append(skills[0]);
+$("#main").append(skills.length);
+
+$("#main").append(numbers);
+numbers.pop();
+$("#main").append(numbers);
+numbers.push(3);
+numbers.push(4);
+$("#main").append(numbers);
+*/
+
+var nameTest = "charLIe chaplIN";
+var finalName;
+console.log(finalName);
+
+var num = 15;
+console.log(String.replace(num, /5/, '2'));
+
